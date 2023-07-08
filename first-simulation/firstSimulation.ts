be9a981981e4908eaad3d650adcf42132892bcd0
Stephen Sparling <sparlingcreations@gmail.com>
2023-07-08 11:13:29 -0400

    remove top level async

--- first-simulation/firstSimulation.ts
+++ first-simulation/firstSimulation.ts
@@ -6,22 +6,26 @@ import './firstSimulation.scss'
 import '../shared/babylon/GUI/guiDialog.scss'
 import Reticle from '../shared/babylon/GUI/Reticle'
 
-const reticle = new Reticle()
-const guiDialog = new GUIDialog()
-const firstPersonScene = await BaseFirstPersonScene.initialize()
+const setup = async () => {
+  const reticle = new Reticle()
+  const guiDialog = new GUIDialog()
+  const firstPersonScene = await BaseFirstPersonScene.initialize()
 
-setupSphereMen(firstPersonScene, guiDialog)
+  setupSphereMen(firstPersonScene, guiDialog)
 
-guiDialog.activate(
-  ['Click to start, WASD to move, no mobile yet sry'],
-  () => {}
-)
+  guiDialog.activate(
+    ['Click to start, WASD to move, no mobile yet sry'],
+    () => {}
+  )
 
-firstPersonScene.engine.runRenderLoop(function () {
-  firstPersonScene.update()
-  reticle.update(firstPersonScene)
-})
+  firstPersonScene.engine.runRenderLoop(function () {
+    firstPersonScene.update()
+    reticle.update(firstPersonScene)
+  })
 
-window.addEventListener('resize', function () {
-  firstPersonScene.engine.resize()
-})
+  window.addEventListener('resize', function () {
+    firstPersonScene.engine.resize()
+  })
+}
+
+setup()
